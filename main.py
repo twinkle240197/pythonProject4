b6ef71ace839f5c84bb17b7ef500b01edd1b6a56
twinkle240197 <twinklecode240197@gmail.com>
2022-09-11 20:41:45 +0530

    command#

--- main.py
+++ main.py
@@ -2,11 +2,48 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-print("happy birthday")
+"""print("happy birthday")
 c="abc"
 d="efg"
 print(c+d)
 
 for i in range(5):
-    print(i)
-# hello
+    print(i) """
+#x=type('23')
+#print(x)
+
+#x=type(eval('23'))
+#print(x)
+
+x=12.3892
+#y=format(x,".2f")
+#print(y)
+
+#a=12345757.464657
+#y=format(a,">4.1f")
+#print(y)
+
+#B=[12,"hi",'orange',24.6]
+#print(B[1:-2])
+
+#C=[12,"hi",'orange',24.6,'Mango','grapes','cat',234,'dog','grapes']
+#C[1:3]='rjv'
+#C.insert(1,'10')
+#print(C)
+#C.remove("24.6")
+#print(C)
+#x1=C.count('grapes')
+#print(x1)
+#print(C)
+z=[1,8,45,98,34,90,23,37]
+Z=sorted(z,reverse=False)
+z.sort()
+#print(list(reversed(z)))
+listB=list((12,13,18))
+#print(listB)
+
+a=()
+print(type(a))
+
+
+
