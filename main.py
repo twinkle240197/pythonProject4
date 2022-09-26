51727cb69a95c6e9fe93fb94ab9efb97631c3851
twinkle240197 <twinklecode240197@gmail.com>
2022-09-26 10:59:46 +0530

    command#

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ for i in range(5):
 #x=type(eval('23'))
 #print(x)
 
-x=12.3892
+#x=12.3892
 #y=format(x,".2f")
 #print(y)
 
@@ -35,15 +35,82 @@ x=12.3892
 #x1=C.count('grapes')
 #print(x1)
 #print(C)
-z=[1,8,45,98,34,90,23,37]
-Z=sorted(z,reverse=False)
-z.sort()
+#z=[1,8,45,98,34,90,23,37]
+#Z=sorted(z,reverse=False)
+#z.sort()
 #print(list(reversed(z)))
-listB=list((12,13,18))
+#listB=list((12,13,18))
 #print(listB)
 
-a=()
-print(type(a))
+#a=()
+#print(type(a))
+
+#k=123456789.654321
+#print(format(k,"<5.1f"))
+
+#z=[1,8,45,98,34,90,23,37]
+#z.sort()
+#print(z
+
+"""lang = input("What's the programming language you want to learn? ")
+
+match lang:
+    case "JavaScript":
+        print("You can become a web developer.")
+
+    case "Python":
+        print("You can become a Data Scientist")
+
+    case "PHP":
+        print("You can become a backend developer")
+
+    case "Solidity":
+        print("You can become a Blockchain developer")
+
+    case "Java":
+        print("You can become a mobile app developer")
+    case _:
+        print("The language doesn't matter, what matters is solving problems.")"""
+
+
+# concatenate
+n1=input("enter the string")
+n2=input("enter the second string")
+print(n1+n2)
+
+#reverse
+n3=input("enter the third string")
+print(n3[::-1])
+
+# string operation
+print(n1[:-1])
+print(n2[0:6])
+print(n1.upper())
+print(n1.lower())
+print(n1.index('n'))
+print(n1.startswith("T"))
+
+s={'3','4','5'}
+t="-"
+print(t.join(s))
+
+# Reverse string function
+def r(a):
+    st=""
+    for i in a:
+     st=i+st
+    return st
+a='twinkle'
+print(r(a))
+
+#concatenate function
+def c(b,d):
+    conca=b+d
+    return conca
+b= "ME in"
+d=" AI"
+print(c(b,d))
+
 
 
 
